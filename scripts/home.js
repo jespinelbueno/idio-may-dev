8d8c80a267585ae97720803062607e02e29bf358
jespinelbueno <76118886+jespinelbueno@users.noreply.github.com>
2026-08-21 15:10:42 -0400

    fixed toolkit circles

--- scripts/home.js
+++ scripts/home.js
@@ -2,14 +2,12 @@ import { initCaseStudyStack } from "./features/case-study-stack.js";
 import { initHomeLoader } from "./features/home-loader.js";
 import { initHomeNav } from "./features/home-nav.js";
 import { initOfferParallax } from "./features/offer-parallax.js";
-import { initToolkitCircles } from "./features/toolkit-circles.js";
 import { initValuesWheel } from "./features/values-wheel/controller.js";
 import { initViewportHeight } from "./features/viewport-height.js";
 
 initViewportHeight();
 initHomeNav();
 initOfferParallax();
-initToolkitCircles();
 initHomeLoader();
 initCaseStudyStack();
 initValuesWheel();
